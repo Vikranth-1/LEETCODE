fe38b85e5f644224955524b84d3b1c0d9c1fdef8
Vikranth V <vvikranth08@gmail.com>
2026-06-17 11:13:38 +0530

    Create 10. Regular Expression Matching.c

---
+++ 10. Regular Expression Matching.c
@@ -0,0 +1,10 @@
+bool isMatch(char* s, char* p) {
+    if (*p == '\0')
+        return *s == '\0';
+    bool firstMatch = (*s != '\0') && (*p == *s || *p == '.');
+    if (*(p + 1) == '*') {
+        return isMatch(s, p + 2) ||
+               (firstMatch && isMatch(s + 1, p));
+    }
+    return firstMatch && isMatch(s + 1, p + 1);
+}
