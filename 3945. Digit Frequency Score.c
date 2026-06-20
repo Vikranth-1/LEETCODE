87c52eea26cd616dcfe4e4acaac662b1c28daf18
Vikranth V <vvikranth08@gmail.com>
2026-06-20 18:55:59 +0530

    Create 3945. Digit Frequency Score.c

---
+++ 3945. Digit Frequency Score.c
@@ -0,0 +1,12 @@
+int digitFrequencyScore(int n) {
+    int freq[10] = {0};
+    while (n > 0) {
+        freq[n % 10]++;
+        n /= 10;
+    }
+    int score = 0;
+    for (int d = 0; d <= 9; d++) {
+        score += d * freq[d];
+    }
+    return score;
+}
