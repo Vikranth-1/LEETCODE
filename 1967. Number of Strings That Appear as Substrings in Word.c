21fc93e53f2712da384bd00268557346637d329f
Vikranth V <vvikranth08@gmail.com>
2026-06-29 06:41:08 +0530

    Create 1967. Number of Strings That Appear as Substrings in Word.c

---
+++ 1967. Number of Strings That Appear as Substrings in Word.c
@@ -0,0 +1,7 @@
+int numOfStrings(char** patterns, int patternsSize, char* word) {
+    int c=0;
+    for(int i=0;i<patternsSize;i++){
+        if(strstr(word,patterns[i])!=NULL ) c++;
+    }
+    return c;
+}
