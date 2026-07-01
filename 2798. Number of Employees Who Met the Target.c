bb442c2af1edbc7d45502b84b198a43d6cdc3385
Vikranth V <vvikranth08@gmail.com>
2026-07-01 06:32:59 +0530

    Create 2798. Number of Employees Who Met the Target.c

---
+++ 2798. Number of Employees Who Met the Target.c
@@ -0,0 +1,7 @@
+int numberOfEmployeesWhoMetTarget(int* hours, int hoursSize, int target) {
+    int c=0;
+    for(int i=0;i<hoursSize;i++){
+        if(hours[i]>=target) c++;
+    }
+    return c;
+}
