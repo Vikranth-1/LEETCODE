52576a22f2e9645468add418dd560c97430e5df0
Vikranth V <vvikranth08@gmail.com>
2026-07-04 15:14:06 +0530

    Create 2220. Minimum Bit Flips to Convert Number.java

---
+++ 2220. Minimum Bit Flips to Convert Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int x=start^goal;
+        int c=0;
+        while(x>0){
+            if((x&1)==1) c++;
+            x>>=1;
+        }
+        return c;
+    }
+}
