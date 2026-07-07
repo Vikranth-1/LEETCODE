beeb4add1e4c7d1eb56b63b81490ebe1b8eb5fb0
Vikranth V <vvikranth08@gmail.com>
2026-07-07 20:38:10 +0530

    Update 283. Move Zeroes.java

--- 283. Move Zeroes.java
+++ 283. Move Zeroes.java
@@ -1,12 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if(nums.length>1){
-            Arrays.sort(nums);
-            while(nums[0]==0){
-                for(int i=0;i<nums.length-1;i++){
-                    nums[i]=nums[i+1];
-                }
-                nums[nums.length-1]=0;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;   
             }
         }
     }
