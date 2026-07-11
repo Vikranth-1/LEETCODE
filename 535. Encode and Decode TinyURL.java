e38691c5037c6816577debcd2515d6dfae15346e
Vikranth V <vvikranth08@gmail.com>
2026-07-11 21:06:46 +0530

    Create 535. Encode and Decode TinyURL.java

---
+++ 535. Encode and Decode TinyURL.java
@@ -0,0 +1,10 @@
+public class Codec {
+
+    public String encode(String longUrl) {
+        return longUrl;
+    }
+
+    public String decode(String shortUrl) {
+        return shortUrl;
+    }
+}
