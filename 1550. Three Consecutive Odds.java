c588ca5218ba9f305055e1fda15398f237290d9a
Vikranth V <vvikranth08@gmail.com>
2026-07-16 16:03:26 +0530

    Create 1550. Three Consecutive Odds.java

---
+++ 1550. Three Consecutive Odds.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int c=0;
+        for(int a: arr){
+            if(a%2==1) c++;
+            else c=0;
+            if(c==3) return true;
+        }
+        return false;
+    }
+}
