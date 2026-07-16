df488a1ac512d47035464bdc8715e4d18bfea1be
Vikranth V <vvikranth08@gmail.com>
2026-07-16 13:43:58 +0530

    Create 2652. Sum Multiples.java

---
+++ 2652. Sum Multiples.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+        int c=0;
+        for(int i=3;i<=n;i++)
+            if(i%3==0 ||i%5==0 ||i%7==0) c+=i;
+        return c;
+    }
+}
