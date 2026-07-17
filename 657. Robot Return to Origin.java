492b8321d211694806f9b3cfc3866fd54dfb7df3
Vikranth V <vvikranth08@gmail.com>
2026-07-17 10:45:46 +0530

    Create 657. Robot Return to Origin.java

---
+++ 657. Robot Return to Origin.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int ud=0,lr=0;
+        for(char c:moves.toCharArray()){
+            if(c=='U') ud++;
+            else if(c=='D') ud--;
+            else if(c=='R') lr++;
+            else if(c=='L') lr--;
+        }
+        return (ud==0&&lr==0);
+    }
+}
