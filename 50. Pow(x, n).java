6e1048a44debf22b14903cbd98d1eacab85d0351
Vikranth V <vvikranth08@gmail.com>
2026-07-21 22:19:42 +0530

    Create 50. Pow(x, n).java

---
+++ 50. Pow(x, n).java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long power=n;
+        double ans=1;
+        if (power<0) {
+            x=1/x;
+            power=-power;
+        }
+        while (power > 0) {
+            if ((power & 1) == 1) {
+                ans *= x;
+            }
+            x*=x;
+            power>>=1;
+        }
+        return ans;
+    }
+}
