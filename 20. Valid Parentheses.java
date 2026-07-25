f0f3fdb7cbc67856749ccf1760c9fbb06c843df0
Vikranth V <vvikranth08@gmail.com>
2026-07-25 10:47:34 +0530

    Create 20. Valid Parentheses.java

---
+++ 20. Valid Parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String str) {
+        if (str.length() <= 1) return false;
+        Stack<Character> stack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
