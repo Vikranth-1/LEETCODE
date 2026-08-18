65b2b93107a3966607420096b9fcca1810e03c21
Vikranth V <vvikranth08@gmail.com>
2026-08-18 19:24:33 +0530

    Create 3099. Harshad Number.java

---
+++ 3099. Harshad Number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int num=x,sum=0;
+        while(x>0){
+            sum+=x%10;
+            x/=10;
+        }
+        return (num%sum==0)?sum:-1;
+    }
+}
