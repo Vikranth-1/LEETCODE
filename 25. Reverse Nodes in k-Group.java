3658a7f054c1ed101ee51867a2bb9ccba5733dae
Vikranth V <vvikranth08@gmail.com>
2026-09-09 15:03:50 +0530

    Create 25. Reverse Nodes in k-Group.java

---
+++ 25. Reverse Nodes in k-Group.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode temp=head;
+        int c=1;
+        while(temp!=null){
+            c++;
+            temp=temp.next;
+        }   
+        for(int i=1;i<=c-k;i+=k)
+            head=reverseBetween(head,i,i+k-1);
+        return head;
+    }
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right) return head;
+        
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        
+        for (int i = 0; i < left - 1; ++i) {
+            prev = prev.next;
+        }
+        
+        ListNode current = prev.next;
+        
+        for (int i = 0; i < right - left; ++i) {
+            ListNode nextNode = current.next;
+            current.next = nextNode.next;
+            nextNode.next = prev.next;
+            prev.next = nextNode;
+        }
+        
+        return dummy.next;
+    }
+}
