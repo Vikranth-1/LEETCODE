724cc251c2b1d8771de6f1851ebaee182e4427cd
Vikranth V <vvikranth08@gmail.com>
2026-09-10 14:31:44 +0530

    Update 496. Next Greater Element I.java

--- 496. Next Greater Element I.java
+++ 496. Next Greater Element I.java
@@ -1,5 +1,27 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] nextGreater=new int[10001];
+        int[] stack=new int[nums2.length];
+        int top=-1;
+        for (int num:nums2) {
+            while (top>=0 && stack[top]<num) {
+                nextGreater[stack[top--]]=num;
+            }
+            stack[++top]=num;
+        }
+        while (top>=0) {
+            nextGreater[stack[top--]]=-1;
+        }
+        int[] ans=new int[nums1.length];
+        for (int i=0;i<nums1.length;i++) {
+            ans[i]=nextGreater[nums1[i]];
+        }
+        return ans;
+    }
+}
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int[] ans=new int[nums1.length];
         for(int i=0;i<nums1.length;i++){
             boolean flag=true;
