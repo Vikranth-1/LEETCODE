bf77dac630a013133de2b2109d44f4fcbb67cd19
Vikranth V <vvikranth08@gmail.com>
2026-09-16 14:57:49 +0530

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -10,3 +10,16 @@ class Solution {
         return new int[]{};
     }
 }
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[]{i,map.get(target-nums[i])};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
