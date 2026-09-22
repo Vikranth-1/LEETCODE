33d4eb563eb22ab4e76f93f319dc6da8bd7b80dc
Vikranth V <vvikranth08@gmail.com>
2026-09-22 14:58:38 +0530

    Create 547. Number of Provinces.java

---
+++ 547. Number of Provinces.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void dfs(int node, int[][] isConnected, boolean[] visited){
+        visited[node] = true;
+        for(int i = 0; i < isConnected.length; i++){
+            if(!visited[i] && isConnected[node][i] == 1){
+                dfs(i, isConnected, visited);
+            }
+        }
+    }
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                dfs(i, isConnected, visited);
+                provinces++;
+            }
+        }
+        return provinces;
+    }
+}
