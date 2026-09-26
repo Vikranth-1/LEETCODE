b8e19c0391c60969c6611981f08d2029d648a75c
Vikranth V <vvikranth08@gmail.com>
2026-09-26 10:58:44 +0530

    Create 125. Valid Palindrome.java

---
+++ 125. Valid Palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb=new StringBuilder();
+        for (char ch : s.toCharArray()) 
+            if (Character.isLetterOrDigit(ch))
+                sb.append(Character.toLowerCase(ch));
+        
+        int l=0,r=sb.length()-1;
+        while(l<r)
+            if(sb.charAt(l++) != sb.charAt(r--)) return false;
+        return true;
+    }
+}
